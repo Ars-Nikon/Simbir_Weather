a57dac6f8cec546089208529b9cac84f92504f56
Penetrati0n <fomenko2002ivan@gmail.com>
2021-06-26 17:29:59 +0300

    Add recomendatition

---
+++ Simbirsoft_Weather/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace Simbirsoft_Weather.Models
+{
+    public class Person
+    {
+        public Clothes Head { get; set; }
+        public Clothes BodyTop { get; set; }
+        public Clothes BodyBottom { get; set; }
+        public Clothes Legs { get; set; }
+        public Clothes Other { get; set; }
+    }
+}

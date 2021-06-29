742ec95a1919816cc06242f886e1cb01449511d2
Penetrati0n <fomenko2002ivan@gmail.com>
2021-06-29 17:20:33 +0300

    Add list with clothes in reccomendation.

--- Simbirsoft_Weather.Tests/Services/ClothingConsultantTest.cs
+++ Simbirsoft_Weather.Tests/Services/ClothingConsultantTest.cs
@@ -1,6 +1,8 @@
-﻿using Simbirsoft_Weather.Models;
+﻿using Moq;
+using Simbirsoft_Weather.Models;
+using Simbirsoft_Weather.Models.Enums;
 using Simbirsoft_Weather.Services;
-using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Simbirsoft_Weather.Tests.Services
@@ -8,17 +10,61 @@ namespace Simbirsoft_Weather.Tests.Services
     public class ClothingConsultantTest
     {
         private readonly IClothingConsultant _clothingConsultant;
+        private readonly List<Clothes> _clothes;
 
         public ClothingConsultantTest()
         {
-            _clothingConsultant = new ClothingConsultant();
+            _clothes = new List<Clothes>()
+            {
+                new Clothes() { ClothesType = ClothesType.Head, Name = "Кепка", MinTemperature = 30, MaxTemperature = 1000, ForWhom = ForWhom.Man },
+                new Clothes() { ClothesType = ClothesType.Head, Name = "Шапка", MinTemperature = -100, MaxTemperature = 10, ForWhom = ForWhom.ForAll },
+                new Clothes() { ClothesType = ClothesType.Head, Name = "Шляпа", MinTemperature = 30, MaxTemperature = 1000, ForWhom = ForWhom.Woman },
+                new Clothes() { ClothesType = ClothesType.BodyTop, Name = "Куртка", MinTemperature = -100, MaxTemperature = 5, ForWhom = ForWhom.Man },
+                new Clothes() { ClothesType = ClothesType.BodyTop, Name = "Пальто", MinTemperature = -100, MaxTemperature = 5, ForWhom = ForWhom.Woman },
+                new Clothes() { ClothesType = ClothesType.BodyTop, Name = "Футболка", MinTemperature = 19, MaxTemperature = 1000, ForWhom = ForWhom.Man },
+                new Clothes() { ClothesType = ClothesType.BodyTop, Name = "Ветровка", MinTemperature = 5, MaxTemperature = 10, ForWhom = ForWhom.ForAll },
+                new Clothes() { ClothesType = ClothesType.BodyTop, Name = "Свитер", MinTemperature = 10, MaxTemperature = 19, ForWhom = ForWhom.ForAll },
+                new Clothes() { ClothesType = ClothesType.BodyTop, Name = "Блузка", MinTemperature = 19, MaxTemperature = 1000, ForWhom = ForWhom.Woman },
+                new Clothes() { ClothesType = ClothesType.BodyBottom, Name = "Джинсы", MinTemperature = 5, MaxTemperature = 25, ForWhom = ForWhom.ForAll },
+                new Clothes() { ClothesType = ClothesType.BodyBottom, Name = "Шорты", MinTemperature = 25, MaxTemperature = 1000, ForWhom = ForWhom.Man },
+                new Clothes() { ClothesType = ClothesType.BodyBottom, Name = "Юбка", MinTemperature = 25, MaxTemperature = 1000, ForWhom = ForWhom.Woman },
+                new Clothes() { ClothesType = ClothesType.BodyBottom, Name = "Теплые штаны", MinTemperature = -100, MaxTemperature = 5, ForWhom = ForWhom.ForAll },
+                new Clothes() { ClothesType = ClothesType.Legs, Name = "Кроссовки", MinTemperature = 15, MaxTemperature = 30, ForWhom = ForWhom.ForAll },
+                new Clothes() { ClothesType = ClothesType.Legs, Name = "Тапочки", MinTemperature = 30, MaxTemperature = 1000, ForWhom = ForWhom.ForAll },
+                new Clothes() { ClothesType = ClothesType.Legs, Name = "Ботинки", MinTemperature = -100, MaxTemperature = 15, ForWhom = ForWhom.ForAll },
+                new Clothes() { ClothesType = ClothesType.Other, Name = "Зонт", MinTemperature = -100, MaxTemperature = 1000, ForWhom = ForWhom.ForAll },
+
+                new Clothes() { ClothesType = ClothesType.Head, Name = "Кепка", MinTemperature = 30, MaxTemperature = 1000, ForWhom = ForWhom.Man },
+                new Clothes() { ClothesType = ClothesType.Head, Name = "Шапка", MinTemperature = -100, MaxTemperature = 10, ForWhom = ForWhom.ForAll },
+                new Clothes() { ClothesType = ClothesType.Head, Name = "Шляпа", MinTemperature = 30, MaxTemperature = 1000, ForWhom = ForWhom.Woman },
+                new Clothes() { ClothesType = ClothesType.BodyTop, Name = "Куртка", MinTemperature = -100, MaxTemperature = 5, ForWhom = ForWhom.Man },
+                new Clothes() { ClothesType = ClothesType.BodyTop, Name = "Пальто", MinTemperature = -100, MaxTemperature = 5, ForWhom = ForWhom.Woman },
+                new Clothes() { ClothesType = ClothesType.BodyTop, Name = "Футболка", MinTemperature = 19, MaxTemperature = 1000, ForWhom = ForWhom.Man },
+                new Clothes() { ClothesType = ClothesType.BodyTop, Name = "Ветровка", MinTemperature = 5, MaxTemperature = 10, ForWhom = ForWhom.ForAll },
+                new Clothes() { ClothesType = ClothesType.BodyTop, Name = "Свитер", MinTemperature = 10, MaxTemperature = 19, ForWhom = ForWhom.ForAll },
+                new Clothes() { ClothesType = ClothesType.BodyTop, Name = "Блузка", MinTemperature = 19, MaxTemperature = 1000, ForWhom = ForWhom.Woman },
+                new Clothes() { ClothesType = ClothesType.BodyBottom, Name = "Джинсы", MinTemperature = 5, MaxTemperature = 25, ForWhom = ForWhom.ForAll },
+                new Clothes() { ClothesType = ClothesType.BodyBottom, Name = "Шорты", MinTemperature = 25, MaxTemperature = 1000, ForWhom = ForWhom.Man },
+                new Clothes() { ClothesType = ClothesType.BodyBottom, Name = "Юбка", MinTemperature = 25, MaxTemperature = 1000, ForWhom = ForWhom.Woman },
+                new Clothes() { ClothesType = ClothesType.BodyBottom, Name = "Теплые штаны", MinTemperature = -100, MaxTemperature = 5, ForWhom = ForWhom.ForAll },
+                new Clothes() { ClothesType = ClothesType.Legs, Name = "Кроссовки", MinTemperature = 15, MaxTemperature = 30, ForWhom = ForWhom.ForAll },
+                new Clothes() { ClothesType = ClothesType.Legs, Name = "Тапочки", MinTemperature = 30, MaxTemperature = 1000, ForWhom = ForWhom.ForAll },
+                new Clothes() { ClothesType = ClothesType.Legs, Name = "Ботинки", MinTemperature = -100, MaxTemperature = 15, ForWhom = ForWhom.ForAll },
+                new Clothes() { ClothesType = ClothesType.Other, Name = "Зонт", MinTemperature = -100, MaxTemperature = 1000, ForWhom = ForWhom.ForAll },
+            };
+         
+            var clothesRepositry = new Mock<IClothesRepository>();
+            clothesRepositry.Setup(repo => repo.GetClothes())
+                .Returns(_clothes);
+
+            _clothingConsultant = new ClothingConsultant(clothesRepositry.Object);
         }
 
         [Theory]
         [InlineData(30, 50, 0.3)]
         public void Test1(double minTemp, double maxTemp, double chanceOfRain)
         {
-            Forecast forecast = new Forecast() { Mintemp = minTemp, Maxtemp = maxTemp, СhanceOfRain = chanceOfRain };
+            Forecast forecast = new Forecast(minTemp, maxTemp, chanceOfRain);
 
             var result = _clothingConsultant.GetRecommendation(forecast);
 
--- Simbirsoft_Weather/Models/Person.cs
+++ Simbirsoft_Weather/Models/Person.cs
@@ -1,11 +1,13 @@
-﻿namespace Simbirsoft_Weather.Models
+﻿using System.Collections.Generic;
+
+namespace Simbirsoft_Weather.Models
 {
     public class Person
     {
-        public Clothes Head { get; set; }
-        public Clothes BodyTop { get; set; }
-        public Clothes BodyBottom { get; set; }
-        public Clothes Legs { get; set; }
-        public Clothes Other { get; set; }
+        public List<Clothes> Head { get; set; }
+        public List<Clothes> BodyTop { get; set; }
+        public List<Clothes> BodyBottom { get; set; }
+        public List<Clothes> Legs { get; set; }
+        public List<Clothes> Other { get; set; }
     }
 }
